13dca2126f90f03f5555a3d99f098e5e40c1b241
sudaratchoothong <62239375+sudaratchoothong@users.noreply.github.com>
2021-12-06 02:17:36 +0700

    updata allow

--- app.js
+++ app.js
@@ -54,13 +54,13 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  next();
-})
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   next();
+// })
 
 // error handler
 app.use(function(err, req, res, next) {
