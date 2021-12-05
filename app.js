4b01e350deb8acaf9f06dad3f8fc0249d65f3a19
sudaratchoothong <62239375+sudaratchoothong@users.noreply.github.com>
2021-12-06 01:16:25 +0700

    updata allow

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   next();
 })
+app.set('trust proxy', 1);
 
 // error handler
 app.use(function(err, req, res, next) {
